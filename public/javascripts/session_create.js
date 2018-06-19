112e9d452249e2dbb7750044b4a0796401f7e21b
Ben Selby <neverbenbetter@gmail.com>
2018-06-19 14:49:17 -0600

    fixed session_create script to display questions - still needs stylin

--- public/javascripts/session_create.js
+++ public/javascripts/session_create.js
@@ -13,15 +13,32 @@ list.addEventListener('change', function() {
     var questions_div = document.getElementById('questions');
 
     // First, delete any old children 
+    while(questions_div.childNodes.length > 0){
+        questions_div.removeChild(questions_div.childNodes[0]);
+    }
     
     // Now add the new kids
     for (i in selected_study.question_set.questions){
-        //var box = document.createElement();
-        //questions_div.appendChild(box);
-        var question_title = document.createTextNode(JSON.parse(selected_study.question_set.questions[i].question_name)); 
-        var question_text = document.createTextNode(JSON.parse(selected_study.question_set.questions[i].description));
-        questions_div.appendChild(question_title);
-        questions_div.appendChild(question_text);
+        var div = document.createElement('div');
+        var question_p = document.createElement('p');
+        var question_title = document.createElement('p')
+            .appendChild(
+                document.createTextNode(selected_study.question_set.questions[i].question_name)); 
+        var question_text = document.createTextNode(selected_study.question_set.questions[i].description);
+        question_p.appendChild(question_text);
+
+        var rating = document.createElement('select');
+        for (var i=0; i<=10; i++){
+            var opt = document.createElement('option');
+            opt.text = i;
+            opt.value = i;
+            rating.appendChild(opt);
+        }
+
+        div.appendChild(question_title);
+        div.appendChild(question_p);
+        div.appendChild(rating);
+        questions_div.appendChild(div);
     }
 });
 
